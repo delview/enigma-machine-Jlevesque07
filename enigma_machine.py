3f54c80e816cac942f615ee02f97ea0bcdeb822c
Jlevesque07 <joshual251@deltalearns.ca>
2025-03-12 10:35:31 -0700

    Started Commenting

---
+++ enigma_machine.py
@@ -0,0 +1,18 @@
+
+
+
+
+# def encoder function 
+# takes the string, makes it a list, translates each letter into morse code, shows the user the encoded message
+
+
+# def decoder function 
+# takes a list of morse codes, translates them into letters, regroups the letters into a string, shows the user the string
+
+import time
+
+print("Yeti")
+
+time.sleep(1)
+
+print("RAh")
