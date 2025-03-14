6b0d23cf3b0fc1c2d9241b7c03a9d9a91b65b3c9
Jlevesque07 <joshual251@deltalearns.ca>
2025-03-14 14:44:37 -0700

    Added list of morse codes

--- enigma_machine.py
+++ enigma_machine.py
@@ -11,5 +11,6 @@
 
 
 # define dictionary for morse code to letter translations
-
+codes = {'a':'.-' , 'b':'-...' , 'c':'-.-.' , 'd':'-..', 'e':'.' , 'f':'..-.' , 'g':'--.' , 'h':'....' , 'i':'..' , 'j':'.---' , 'k':'-.-' , 'l':'.-..' , 'm':'--' , 'n':'-.' , 'o':'---' , 'p':'.--.' , 'q':'--.-' , 'r':'.-.' , 's':'...' , 't':'-' , 'u':'..-' , 'v':'...-' , 'w':'.--' , 'x':'-..-' , 'y':'-.--' , 'z':'--..'}
+ 
 # user interface, options to encode or decode
