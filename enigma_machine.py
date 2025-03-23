acca24dc254df0bac84ed4d9b8ace476437b833f
Jlevesque07 <joshual251@deltalearns.ca>
2025-03-23 03:21:21 +0000

    Created string encoder. (Part of encode function)

--- enigma_machine.py
+++ enigma_machine.py
@@ -4,13 +4,67 @@
 
 # def encoder function 
 # takes the string, makes it a list, translates each letter into morse code, shows the user the encoded message
-
-
+def encode(string: str) -> list:
+    encoded_message = []
+    for letter in string:
+        if letter == 'a':
+            encoded_message.append(".-")
+        elif letter == 'b':  
+            encoded_message.append("-...")
+        elif letter == 'c':
+            encoded_message.append("-.-.")
+        elif letter == 'd':
+            encoded_message.append("-..")
+        elif letter == 'e':
+            encoded_message.append(".")
+        elif letter == 'f':
+            encoded_message.append("--.")
+        elif letter == 'g':
+            encoded_message.append("-...")
+        elif letter == 'h':
+            encoded_message.append("....")
+        elif letter == 'i':
+            encoded_message.append("..")
+        elif letter == 'j':
+            encoded_message.append(".---")
+        elif letter == 'k':
+            encoded_message.append("-.-")
+        elif letter == 'l':
+            encoded_message.append(".-..")
+        elif letter == 'm':
+            encoded_message.append("--")
+        elif letter == 'n':
+            encoded_message.append("-.")
+        elif letter == 'o':
+            encoded_message.append("---")
+        elif letter == 'p':
+            encoded_message.append(".--.")
+        elif letter == 'q':
+            encoded_message.append("--.-")
+        elif letter == 'r':
+            encoded_message.append(".-.")
+        elif letter == 's':
+            encoded_message.append("...")
+        elif letter == 't':
+            encoded_message.append("-")
+        elif letter == 'u':
+            encoded_message.append("..-")
+        elif letter == 'v':
+            encoded_message.append("...-")
+        elif letter == 'w':
+            encoded_message.append(".--")
+        elif letter == 'x':
+            encoded_message.append("-..-")
+        elif letter == 'y':
+            encoded_message.append("-.--")
+        elif letter == 'z':                      
+            encoded_message.append("--..")
+    print(encoded_message)
 # def decoder function 
 # takes a list of morse codes, translates them into letters, regroups the letters into a string, shows the user the string
 
 
 # define dictionary for morse code to letter translations
-codes = {'a':'.-' , 'b':'-...' , 'c':'-.-.' , 'd':'-..', 'e':'.' , 'f':'..-.' , 'g':'--.' , 'h':'....' , 'i':'..' , 'j':'.---' , 'k':'-.-' , 'l':'.-..' , 'm':'--' , 'n':'-.' , 'o':'---' , 'p':'.--.' , 'q':'--.-' , 'r':'.-.' , 's':'...' , 't':'-' , 'u':'..-' , 'v':'...-' , 'w':'.--' , 'x':'-..-' , 'y':'-.--' , 'z':'--..'}
- 
+string = input("Input a message. ")
+encode(string)
 # user interface, options to encode or decode
